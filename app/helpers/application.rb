576f1a6faabe11a1f4aaafc4c5cbd007b75a784f
marcin walendzik <marcin.walendzik@gmail.com>
2015-01-30 16:04:22 +0000

    adding json in helpers

--- app/helpers/application.rb
+++ app/helpers/application.rb
@@ -2,4 +2,8 @@ helpers do
   def get_quote(cross)
     return YahooFinance.quote(["#{cross}"],[:bid,:ask,:last_trade_price])
   end  
+
+  def send_hash_to_jason(hash)
+    return json(hash, :encoder => :to_json,:content_type => :js)
+  end  
 end
